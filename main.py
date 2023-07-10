e61b1074afd6c48bd7b6a31daeb1f9d317687cfc
slarelerouge <slarelerouge@users.noreply.github.com>
2023-07-10 12:17:49 +0200

    some fixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import configparser
 import os
 import time
 
-
+# TODO subtask test and reporter not working
 # FUNCTIONS
 def is_excel_recent():
     _input = "n"
@@ -15,7 +15,6 @@ def is_excel_recent():
         print("THE EXCEL IS OLD, YOU SURELY")
         print("FORGOT TO COPY IT FROM CLOUD")
         print("PRESS Y TO CONTINUE")
-        print("y/n")
         while _input != "y":
             _input = input()
         print("___________________________")
@@ -36,7 +35,7 @@ def get_user_id(email):
     return list_search[0].accountId
 
 
-def create_jira(project, issuetype, summary, assignee=None, description=None, parent=None, components=None, priority=None, estimate=None):
+def create_jira(project, issuetype, summary, assignee=None, description=None, parent=None, components=None, priority=None, estimate=None, reporter=None):
     fields = {"project": {"key": project}, "issuetype": {"name": issuetype}, "summary": summary}
     if assignee is not None:
         jira_user_id = get_user_id(assignee)
@@ -49,9 +48,12 @@ def create_jira(project, issuetype, summary, assignee=None, description=None, pa
         fields["components"] = [{"add": comp} for comp in components]
     if priority is not None:
         fields["priority"] = {"name": priority.capitalize()}
+    if reporter is not None:
+        reporter_jira_user_id = get_user_id(reporter)
+        fields["reporter"] = {"accountId": reporter_jira_user_id}
 
     _issue = jira.create_issue(fields=fields)
-    _issue.update(fields={"timetracking": {"originalEstimate": "1w"}})
+    _issue.update(fields={"timetracking": {"originalEstimate": estimate}})
     return _issue
 
 
@@ -112,27 +114,42 @@ worksheet = wb_obj.get_sheet_by_name(SPRITESHEET_NAME)
 
 last_story = None
 last_epic = None
+last_epic_assignee = None
+last_task = None
 
 
 for row in range(START_ROW, worksheet.max_row+1):#worksheet.max_row+1
     #for column in "ADEF":  #Here you can add or reduce the columns
     cell_name = "{}{}".format(SUMMARY_COLUMN, row)
     summary_cell = worksheet[cell_name] # the value of the specific cell
-    if summary_cell.value is not None:
-        cell_name = "{}{}".format(ISSUETYPE_COLUMN, row)
-        issuetype_cell = worksheet[cell_name]
-        if issuetype_cell.value == "Epic":
-            cell_name = "{}{}".format(KEY_COLUMN, row)
-            key_cell = worksheet[cell_name]
-            if key_cell.value is not None:
-                last_epic = key_cell.value
-            last_story = None
-        elif issuetype_cell.value == "Story":
-            cell_name = "{}{}".format(KEY_COLUMN, row)
-            key_cell = worksheet[cell_name]
-            if key_cell.value is not None:
-                last_story = key_cell.value
+    cell_name = "{}{}".format(ASSIGNEE_COLUMN, row)
+    assignee_cell = worksheet[cell_name]
 
+    if summary_cell.value is not None:
+        color = f"_{summary_cell.fill.start_color.index}_"[1:-1]
+        if color in VALIDATION_COLOR:
+            cell_name = "{}{}".format(ISSUETYPE_COLUMN, row)
+            issuetype_cell = worksheet[cell_name]
+            if issuetype_cell.value == "Epic":
+                cell_name = "{}{}".format(KEY_COLUMN, row)
+                key_cell = worksheet[cell_name]
+                if key_cell.value is not None:
+                    last_epic = key_cell.value
+                    last_epic_assignee = assignee_cell.value
+
+                last_story = None
+                last_task = None
+
+            elif issuetype_cell.value == "Story":
+                cell_name = "{}{}".format(KEY_COLUMN, row)
+                key_cell = worksheet[cell_name]
+                if key_cell.value is not None:
+                    last_story = key_cell.value
+            elif issuetype_cell.value == "Task":
+                cell_name = "{}{}".format(KEY_COLUMN, row)
+                key_cell = worksheet[cell_name]
+                if key_cell.value is not None:
+                    last_task = key_cell.value
 
         color = f"_{summary_cell.fill.start_color.index}_"[1:-1]
         if color in VALIDATION_COLOR:
@@ -141,46 +158,76 @@ for row in range(START_ROW, worksheet.max_row+1):#worksheet.max_row+1
             if key_cell.value is None:
                 cell_name = "{}{}".format(DESCRIPTION_COLUMN, row)
                 description_cell = worksheet[cell_name]
-                cell_name = "{}{}".format(ASSIGNEE_COLUMN, row)
-                assignee_cell = worksheet[cell_name]
                 cell_name = "{}{}".format(PRIORITY_COLUMN, row)
                 priority_cell = worksheet[cell_name]
                 cell_name = "{}{}".format(ESTIMATE_COLUMN, row)
                 estimate_cell = worksheet[cell_name]
 
-                if issuetype_cell.value == "Epic":
-                    new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
-                                            description=description_cell.value, assignee=assignee_cell.value,
-                                            priority=priority_cell.value)
+                if issuetype_cell.value is not None and assignee_cell.value is not None and priority_cell.value is not None:
 
-                    last_epic = str(new_issue)
+                    if issuetype_cell.value == "Epic":
+                        new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
+                                                description=description_cell.value, assignee=assignee_cell.value,
+                                                priority=priority_cell.value, reporter=assignee_cell.value)
 
-                elif issuetype_cell.value == "Story":
-                    new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
-                                            description=description_cell.value, assignee=assignee_cell.value,
-                                            priority=priority_cell.value, parent=last_epic)
+                        last_epic = str(new_issue)
+                        last_story = None
+                        last_task = None
 
-                    last_story = str(new_issue)
+                    elif issuetype_cell.value == "Story":
+                        new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
+                                                description=description_cell.value, assignee=assignee_cell.value,
+                                                priority=priority_cell.value, parent=last_epic, reporter=last_epic_assignee)
 
+                        last_story = str(new_issue)
+
+                    elif issuetype_cell.value == "Task":
+                        if last_epic is not None:
+                            new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
+                                                    description=description_cell.value, assignee=assignee_cell.value,
+                                                    priority=priority_cell.value, parent=last_epic, estimate=estimate_cell.value, reporter=last_epic_assignee)
+                        else:
+                            new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
+                                                    description=description_cell.value, assignee=assignee_cell.value,
+                                                    priority=priority_cell.value, estimate=estimate_cell.value)
+                        if last_story is not None:
+                            jira.create_issue_link(
+                                type = "Relates",
+                                inwardIssue = last_story,
+                                outwardIssue = str(new_issue)
+                            )
+                        last_task = key_cell.value
+
+                    elif issuetype_cell.value == "Sub-task":
+                        if last_epic is not None:
+                            new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
+                                                    description=description_cell.value, assignee=assignee_cell.value,
+                                                    priority=priority_cell.value, parent=last_task, estimate=estimate_cell.value, reporter=last_epic_assignee)
+                        else:
+                            new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
+                                                    description=description_cell.value, assignee=assignee_cell.value,
+                                                    priority=priority_cell.value, estimate=estimate_cell.value)
+                        if last_story is not None:
+                            jira.create_issue_link(
+                                type = "Relates",
+                                inwardIssue = last_story,
+                                outwardIssue = str(new_issue)
+                            )
+
+
+                    hyper_link = get_issue_hyperlink(new_issue)
+                    key_cell.value = str(new_issue)
+                    key_cell.hyperlink = hyper_link
+                    key_cell.style = "Hyperlink"
+                    print("Created issue: ", new_issue)
+                    written = False
+                    while written is False:
+                        try:
+                            wb_obj.save(EXCEL_LOCATION)
+                            written = True
+                        except:
+                            print("Xlsx file seems to be open")
+                            print("Press a Enter to continue")
+                            input()
                 else:
-                    if last_epic is not None:
-                        new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
-                                                description=description_cell.value, assignee=assignee_cell.value,
-                                                priority=priority_cell.value, parent=last_epic, estimate=estimate_cell.value)
-                    else:
-                        new_issue = create_jira(PROJECT, issuetype_cell.value, summary_cell.value,
-                                                description=description_cell.value, assignee=assignee_cell.value,
-                                                priority=priority_cell.value, estimate=estimate_cell.value)
-                    if last_story is not None:
-                        jira.create_issue_link(
-                            type = "Relates",
-                            inwardIssue = last_story,
-                            outwardIssue = str(new_issue)
-                        )
-                hyper_link = get_issue_hyperlink(new_issue)
-                key_cell.value = str(new_issue)
-                key_cell.hyperlink = hyper_link
-                key_cell.style = "Hyperlink"
-                print("Created issue: ", new_issue)
-
-wb_obj.save(EXCEL_LOCATION)
+                    print(f"row {row} has incomplete definition")
